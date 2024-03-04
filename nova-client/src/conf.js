cb9cc85b94eb7b7ac44c4d8c14f21b3c827090b9
ภูณฤทธิ์ ฤทธิรัตน์ <poohnarit23@gmail.com>
2024-03-05 01:49:29 +0700

    Change API path to production

--- nova-client/src/conf.js
+++ nova-client/src/conf.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
     isProd,
-    apiPrefix: isProd ? 'https://stellar-store-416210.as.r.appspot.com' : 'http://localhost:1337'
+    apiPrefix: isProd ? 'https://stellar-store-416210.as.r.appspot.com' : 'https://stellar-store-416210.as.r.appspot.com'
 }
 
 export default config;
